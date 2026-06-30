14bc50a1d8d9d8dbb00f85321e65ed84002d1c5c
Satish01-oss <rs1599696@gamil.com>
2026-06-30 19:21:33 +0530

    changed DB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const app = require('./src/app');
 const db = require('./src/db/db');
-db();
 
 app.listen( process.env.PORT || 3000, () => {
   console.log('Server is running on port ' + (process.env.PORT || 3000))
+  db();
 })
