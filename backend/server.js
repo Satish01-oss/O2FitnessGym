0cde89b7734139bcad186593da645159d9605d80
Satish01-oss <rs1599696@gamil.com>
2026-06-30 17:39:05 +0530

    Use Render PORT environment variable

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const app = require('./src/app');
 const db = require('./src/db/db');
+db();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-  db();
+app.listen( process.env.PORT || 3000, () => {
+  console.log('Server is running on port ' + (process.env.PORT || 3000))
 })
